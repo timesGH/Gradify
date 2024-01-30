d7ea5927ab66b104eb69d580b0489e37aecef6b0
Bilguun <bilguun1324@gmail.com>
2024-01-30 16:32:01 +1100

    Refactor thread card component and add heart SVG

--- src/pages/student-discussion-forum.tsx
+++ src/pages/student-discussion-forum.tsx
@@ -1,30 +1,59 @@
-import { Box, List, Paper } from "@mui/material";
+import { Box, List } from "@mui/material";
 import { AdminForumCard, ThreadCard } from "../components";
 
 export const StudentDiscussionForum = () => {
+  const DEMA_THREADS = [
+    {
+      name: "The Philosophy of Science",
+      src: "gradify/science",
+      reaction: 37,
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vitae elit libero, a pharetra augue. Nullam id dolor id nibh ultricies vehicula ut id elit.",
+    },
+    {
+      name: "The Philosophy of Humanities",
+      src: "gradify/humanities",
+      reaction: 15,
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vitae elit libero, a pharetra augue. Nullam id dolor id nibh ultricies vehicula ut id elit.",
+    },
+    {
+      name: "The Philosophy of Science",
+      src: "gradify/science",
+      reaction: 37,
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vitae elit libero, a pharetra augue. Nullam id dolor id nibh ultricies vehicula ut id elit.",
+    },
+    {
+      name: "The Philosophy of Humanities",
+      src: "gradify/humanities",
+      reaction: 15,
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vitae elit libero, a pharetra augue. Nullam id dolor id nibh ultricies vehicula ut id elit.",
+    },
+  ];
+
   return (
     <Box
       width="100vw"
       height="100vh"
       display="flex"
-      alignItems="center"
       justifyContent="space-evenly"
+      flexWrap="wrap"
+      alignItems="center"
+      overflow="auto"
     >
       <AdminForumCard />
-      <List>
-        <ThreadCard
-          name="The Philosophy of Science"
-          src="gradify/science"
-          reaction={37}
-          defination="blah blha blah"
-        />
-        <ThreadCard
-          name="The Philosophy of Humanities"
-          src="gradify/humanities"
-          reaction={15}
-          defination="blah blha blah"
-        />
-      </List>
+      {DEMA_THREADS.map((thread) => {
+        return (
+          <ThreadCard
+            name={thread.name}
+            src={thread.src}
+            reaction={thread.reaction}
+            description={thread.description}
+          />
+        );
+      })}
     </Box>
   );
 };
